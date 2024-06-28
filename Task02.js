90680d151e5a3bfc179d8de51390c8c878ed7e1c
Степан Рудаков <19stepan94@mail.ru>
2024-06-28 14:13:05 +0500

    сделал вторую задачу в 4.5

--- Task02.js
+++ Task02.js
@@ -1,1 +1,19 @@
-'use strict'
+  'use strict'
+
+  
+  const isPrime = (num) => {
+    if (num > 1) {
+      for (let x = 2; x < num; x++) {
+        if (num % x === 0) {
+         return false;
+        } 
+      }
+         return true;
+       } else {
+          return false;
+         } 
+       } 
+
+ 
+  const primeNumber = isPrime(101)
+  console.log('Простое число:' , primeNumber);
